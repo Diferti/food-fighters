c3f4ea4d092ad5f9ee68f4530b6d194f9af7d3d5
Danial Jumagaliyev <djumagaliyev@myseneca.ca>
2025-03-20 14:13:54 -0400

    analysis page

---
+++ app/(img)/analysis.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const insertAi = () => {
+    
+};
+
+const analysis = () => {
+  return (
+    <SafeAreaView>
+        <View>
+            <Text>analysis</Text>
+        </View>
+    </SafeAreaView>
+  )
+};
+
+export default analysis
